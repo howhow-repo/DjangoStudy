1edd4e591fb34e41e3ff55912d04a512ac6cf0dc
howhow-repo <howard.hsu1129@gmail.com>
2021-03-05 18:26:17 +0800

    base templates into base.html; add backToIndex button in challenge page

--- challenges/urls.py
+++ challenges/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("",views.index),
+    path("",views.index,name="challenges-index"),
     path("<int:month>",views.int_month_rederict),
     path("<str:month>",views.month_challenges,name = "month-challenges")
 ]
